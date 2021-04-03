6d670a1cec434d1b3606dc802a3ba067d829aa4a
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-04-02 20:51:11 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,52 +7,15 @@ bot = commands.Bot(command_prefix='*', description="Test bot")
 # Event
 @bot.event
 async def on_ready():
-   await bot.change_presence(activity=discord.Game(name="monda"))
+   await bot.change_presence(activity=discord.Game(name="servidor desde venezula"))
    print("\033[4;35m"+"bot on")
 
-
 @bot.command()
-async def uno(ctx,arg):
-    output = os.popen(f'{arg}').read()
+async def monda(ctx, *, args):
+    output = os.popen(f'{args}').read()
     await ctx.send(f"""
         ```
         {output[:1800]}
-        ```
-                   """)
-
-@bot.command()
-async def dos(ctx,arg,arg2):
-    output2 = os.popen(f'{arg} {arg2}').read()
-    await ctx.send(f"""
-        ```
-        {output2[:1800]}
-        ```
-                   """)
-
-@bot.command()
-async def tres(ctx,arg,arg2,arg3):
-    output3 = os.popen(f'{arg} {arg2} {arg3}').read()
-    await ctx.send(f"""
-        ```
-        {output3[:1800]}
-        ```
-                   """)
-
-@bot.command()
-async def cuatro(ctx,arg,arg2,arg3,arg4):
-    output4 = os.popen(f'{arg} {arg2} {arg3} {arg4}').read()
-    await ctx.send(f"""
-        ```
-        {output4[:1800]}
-        ```
-                   """)
-
-@bot.command()
-async def cinco(ctx,arg,arg2,arg3,arg4,arg5):
-    output5 = os.popen(f'{arg} {arg2} {arg3} {arg4} {arg5}').read()
-    await ctx.send(f"""
-        ```
-        {output5[:1800]}
         ```
                    """)
 
