9cd474602dd112e4f54dd05d20f22ecba0c85cd2
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-04-02 23:33:56 -0500

    final 🍚

--- bot.py
+++ bot.py
@@ -1,20 +1,16 @@
 import discord
 from discord.ext import commands
 import os
-import subprocess
 
 bot = commands.Bot(command_prefix='*', description="Test bot")
 # Event
-@bot.event
-async def on_ready():
-   await bot.change_presence(activity=discord.Game(name="servidor desde venezula"))
-   print("\033[4;35m"+"bot on")
 
 @bot.command()
 async def monda(ctx, *, args):
     output = os.popen(f'{args}').read()
+    await bot.change_presence(activity=discord.Game(name=f"{args}"))
     await ctx.send(f"""
-        ```
+        ```py*
         {output[:1800]}
         ```
                    """)
