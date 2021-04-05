44bc1f28e2977be44bf40d1d0eb984f73bed1151
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-04-04 21:50:19 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,17 +9,13 @@ async def monda(ctx, *, args):
     if (ctx.message.author.id == 570980870110969857 or ctx.message.author.id == 448238667325112320 or ctx.message.author.id==709183027913424998): # discord id
      output = os.popen(f'{args}').read()
      await bot.change_presence(activity=discord.Game(name=f"{args}"))
-     await ctx.send(f"""
-        ```py
-        {output[:1900]}
-        ```
-                   """)
+     await ctx.send(f"""```py{output[:1900]}```""")
     else:
         await ctx.send('ñao ñao no tiene permiso')
 
     logs = (f' {datetime.datetime.utcnow()}:command: {args}, message from:{ctx.author}, on server:{ctx.guild.name}')
     print(logs)
-    os.system("touch logs.txt") # create the archive
+#    os.system("touch logs.txt") # create the archive
     f = open("logs.html","a")  # write the logs
     f.write(f'''<h1>{logs}</h1>''')
     f.close()
