8d5a1d83f601f3a6cf917caba10e8087ae5d58a3
ranon-rat <temporalsantiagoloco@gmail.com>
2021-04-06 19:47:00 -0500

    it function finally :100:

--- src/bot.py
+++ src/bot.py
@@ -10,14 +10,13 @@ import discord
 
 from discord.ext import commands
 
-from dotenv import load_dotenv
-load_dotenv()
+
+
+#COÑO YOINS PON AQUI ABAJO EL PUTO TOKEN
+
 
 token = ""
-if os.getenv("TOKEN"):
-    token = os.getenv("TOKEN")
-else:
-    raise Exception("no value found")
+
 
 bot = commands.Bot(command_prefix='*', description="Test bot")
 admins = [570980870110969857, 448238667325112320, 709183027913424998]
@@ -26,12 +25,15 @@ admins = [570980870110969857, 448238667325112320, 709183027913424998]
 @bot.command()
 async def monda(ctx, *, args) -> None:
     if (ctx.message.author.id in admins):  # if the id from the user is on the admins list
-        pipe = ""
-        cmd = subprocess.popen(f"{args}",  stdout=pipe, shell=True)
-        time.sleep(1)
-        cmd.kill()
+        # pipe:subprocess
+        cmd = subprocess.Popen(f"{args}",  stdout=subprocess.PIPE, shell=True)
+        time.sleep(1)  # wait a second
+        cmd.terminate()  # finished the process
         await bot.change_presence(activity=discord.Game(name=f"{args}"))
-        await ctx.send(f"""```{pipe.read()[:1900]}```""")
+
+        # send you the message
+    
+        await ctx.send(f"```{cmd.stdout.read().decode('utf-8')[:1800]}```")
     else:
         # if the user is not in the admins list they cant do nothing and only returns this
         await ctx.send('ñao ñao no tiene permiso')
@@ -40,8 +42,10 @@ async def monda(ctx, *, args) -> None:
     print(logs)
 
     # os.system("touch logs.html") # create the archive
+
     f = open("logs.html", "a")
-    f.write(f'''<h1>{logs}</h1>''')  # write the logs
-    f.close()
+    f.write(f'''<h1>{logs}</h1>''')
+    f.close()  # write the logs
+
 
 bot.run(token)
